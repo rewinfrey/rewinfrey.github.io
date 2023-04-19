65e87fdfd348057f17342a66bbc82cb4c8a31c22
Rick Winfrey <rewinfrey@github.com>
2023-04-18 22:38:13 -0700

    Initial promise demo

---
+++ assets/javascripts/promise-demo.js
@@ -0,0 +1,55 @@
+const PromiseDemo = {
+  consoleLog:
+    ((msg) =>
+      (resolve) => {
+        console.log(msg);
+        return resolve();
+      }
+    ),
+
+  withPromise:
+    function (op) {
+      return new Promise((resolve, reject) => op(resolve, reject));
+    },
+
+  sleep: function(ms, resolve) {
+    return ((resolve) => setTimeout(resolve, ms));
+  },
+
+  main2: async function() {
+    await this.withPromise((stop) => this.consoleLog("promise one")(stop));
+    await this.withPromise((stop) => this.sleep(1000)(stop));
+    await this.withPromise((stop) => this.consoleLog("promise two")(stop));
+    await this.withPromise((stop) => this.sleep(1000)(stop));
+    await this.withPromise((stop) => this.consoleLog("promise three")(stop));
+  },
+
+  main: async function() {
+    this.withPromise(
+      (stopOne) => {
+        this.withPromise(
+          (stopTwo) => {
+            return (this.consoleLog("innerPromise"))(stopTwo);
+          },
+          () => {
+            this.withPromise(
+              async (stopThree) => {
+                await this.sleep(2);
+                return (this.consoleLog("third promise"))(stopThree);
+              },
+              () => {}
+            );
+            console.log("outerPromise");
+            setTimeout(() => {
+              console.log("withPromise setTimeout");
+              stopOne();
+            }, 10);
+          }
+        )
+      },
+      () => console.log("main promise resolved")
+    )
+  },
+};
+
+PromiseDemo.main2()
