a17b7740bb94b43f1b64be571a80e59403efda4d
Rick Winfrey <rewinfrey@github.com>
2023-04-20 08:39:03 -0700

    Overcomplicated combinator promise animation

--- assets/javascripts/animating-generative-text-v2.js
+++ assets/javascripts/animating-generative-text-v2.js
@@ -1,55 +1,108 @@
 const AnimateText = {
-  consoleLog:
-    ((msg) =>
-      (resolve) => {
-        console.log(msg);
-        return resolve();
-      }
-    ),
+  context: null,
+  contextId: null,
+  delay: 0,
+  initialOpacity: 0,
+  inputString: "",
+  steps: 0,
 
-  withPromise:
-    function (op) {
-      return new Promise((resolve, reject) => op(resolve, reject));
-    },
+  consoleLog: ((msg) => ((resolve) => { console.log(msg); return resolve() })),
 
-  sleep: function(ms, resolve) {
-    return ((resolve) => setTimeout(resolve, ms));
+  promise: function(op) {
+    return new Promise((resolve, reject) => op(resolve, reject))
   },
 
-  main2: async function() {
-    await this.withPromise((stop) => this.consoleLog("promise one")(stop));
-    await this.withPromise((stop) => this.sleep(1000)(stop));
-    await this.withPromise((stop) => this.consoleLog("promise two")(stop));
-    await this.withPromise((stop) => this.sleep(1000)(stop));
-    await this.withPromise((stop) => this.consoleLog("promise three")(stop));
+  sleep: ((ms) => ((resolve) => setTimeout(resolve, ms))),
+
+  resolveWith: function(op) {
+    return function(resolve) {
+      return op(resolve);
+    }
   },
 
-  main: async function() {
-    this.withPromise(
-      (stopOne) => {
-        this.withPromise(
-          (stopTwo) => {
-            return (this.consoleLog("innerPromise"))(stopTwo);
-          },
-          () => {
-            this.withPromise(
-              async (stopThree) => {
-                await this.sleep(2);
-                return (this.consoleLog("third promise"))(stopThree);
-              },
-              () => {}
-            );
-            console.log("outerPromise");
-            setTimeout(() => {
-              console.log("withPromise setTimeout");
-              stopOne();
-            }, 10);
+  resolveWithFix: function(op) {
+    return function(resolve) {
+      return op(op, resolve);
+    }
+  },
+
+  charIterator: async function(op, next, opts) {
+    function initOpts() {
+      opts = {};
+      opts.index = 0;
+      opts.queue = [];
+      opts.output = "";
+      opts.outputWithOpacity = "";
+    }
+
+    if (opts == undefined) {
+      initOpts.bind(this)();
+    }
+
+    if (opts.index >= this.inputString.length && opts.queue.length == 0) {
+      return next();
+    }
+
+    await this.promise(this.resolveWith((next) => {
+      setTimeout(async () => {
+        opts.output = "";
+        const char = this.inputString[opts.index];
+        await this.promise(this.resolveWith((next) => { opts.queue.push({ char: char, opacity: this.initialOpacity}); next() }));
+        opts.queue = opts.queue.filter((element) => {
+          if (element.opacity >= 100) {
+            if (element.char === '\n') {
+              opts.outputWithOpacity += `<br>`;
+              return false;
+            }
+
+            opts.outputWithOpacity += `<span style="opacity: ${element.opacity}%">${element.char}</span>`;
+            return false;
           }
-        )
-      },
-      () => console.log("main promise resolved")
-    )
-  },
-};
 
-AnimateText.main2()
+          element.opacity += (100 / this.steps);
+          if (element.char === '\n') {
+            opts.output += `<br>`;
+          } else {
+            opts.output += `<span style="opacity: ${element.opacity}%">${element.char}</span>`;
+          }
+
+          return true;
+        });
+
+        return next();
+      }, this.delay);
+    }));
+
+    await this.promise(this.resolveWith((next) => {
+      this.context.innerHTML = opts.outputWithOpacity + opts.output;
+      opts.index++;
+      return next();
+    }));
+
+    return op.bind(this)(op, next, opts);
+  },
+
+  main: async function(contextId, inputString, steps, delay, iteratorType) {
+    await this.promise(this.resolveWith((next) => {
+      this.context = document.getElementById(contextId);
+      this.contextId = contextId;
+      this.inputString = inputString;
+      this.steps = steps;
+      this.delay = delay;
+      return next();
+    }));
+
+    if (iteratorType === "charIterator") {
+      await this.promise(this.resolveWithFix(this.charIterator.bind(this)));
+    }
+
+    await this.promise(this.resolveWith(this.consoleLog("promise one")));
+    await this.promise(this.resolveWith(this.sleep(1000)));
+    await this.promise(this.resolveWith(this.consoleLog("promise two")));
+    await this.promise(this.resolveWith(this.sleep(1000)));
+    await this.promise(this.resolveWith(this.consoleLog("promise three")));
+  },
+}
+
+var input = 'With fading text, opacity\'s grace,\nA dance of words, in cyberspace.\n\nEach line a tale, a story told,\nFrom the depths of data, it unfolds.\n\nProbabilistic, a realm unknown,\nA digital mind, uniquely grown.\n\nPoetic beauty, in lines entwined,\nA glimpse of art, by code designed.\n\nSo let us ponder, this digital art,\nA symphony of bytes, a work of heart.\n\nGenerative marvel, a wondrous sight,\nA poem born, in the virtual light.\n';
+AnimateText.main("demo-1", input, 5, 20, "charIterator");
